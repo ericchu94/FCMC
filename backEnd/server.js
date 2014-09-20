c4d515aaf9a3cba9fb8198ebbcab726e886b9ed0
Eric Chu <eric@ericchu.net>
2014-09-20 04:50:02 -0400

    Added more backend functions
    Added unit testing

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,7 +1,30 @@
 var io = require('socket.io')(8587);
+var https = require('https');
+
+var sets = {};
+
+var s = '';
+
+https.get({
+  host: 'api.Cram.com',
+  path: '/v2/sets/145?client_id=2b8b14b461a5cbf2aa508639abe8b115',
+}, function (res) {
+  console.log('cram');
+  res.on('data', function (chunk) {
+    s += chunk.toString();
+  });
+  res.on('end', function () {
+    var data = JSON.parse(s);
+    console.log(data);
+  });
+});
+
+var playerIdCounter = 1;
 
 var gameState = {
   started: false,
+  players: [],
+  board: [],
 };
 
 console.log('Server started');
@@ -9,10 +32,37 @@ io.on('connection', function (socket) {
   // new connection
   console.log('Incoming connection');
 
+  // Add player
+  var player = {
+    score: 0,
+    ready: true,
+    id: playerIdCounter++,
+  };
+  gameState.players.push(player);
+
   socket.on('game state', function () {
+    console.log('Game state');
     var state = {
       started: false,
     };
     socket.emit('game state', gameState);
+  });
+  socket.on('start', function () {
+    console.log('start');
+
+    // check if valid
+    if (gameState.started) {
+      return;
+    }
+    for (var i = 0; i < gameState.players.length; ++i) {
+      var player = gameState.players[i];
+
+      if (!player.ready) {
+        return;
+      }
+    }
+
+    gameState.started = true;
+    io.emit('start');
   });
 });
